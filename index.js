9dc0c21ec7ba5ae1be2ea4e695c20c32dc07899b
mike-degreat <okoronum@gmail.com>
2024-01-28 14:20:11 +0100

    get movies from storage

--- index.js
+++ index.js
@@ -19,3 +19,18 @@ class Movie {
       this.availableCopies = availableCopies;
     }
 }
+
+// Storage functions (replace with your actual storage implementation)
+function getMoviesFromStorage() {
+    const movies = moviesData.map((movieData) => {
+      return new Movie(
+        movieData.id,
+        movieData.title,
+        movieData.genre,
+        movieData.year, // Use 'year' to match the property in moviesData
+        movieData.availableCopies
+      );
+    });
+    return movies;
+}
+
